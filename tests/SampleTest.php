44d27155002d7301f664af9bf4040550539c3f9e
Chris Penny <chris.penny@gmail.com>
2025-07-07 08:42:18 +1200

    Testing queued jobs in GHA workflow

---
+++ tests/SampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SilverStripe\ForagerSubsites\Tests;
+
+use SilverStripe\Dev\SapphireTest;
+
+class SampleTest extends SapphireTest
+{
+
+    public function testSample(): void
+    {
+        $this->assertTrue(true);
+    }
+
+}
